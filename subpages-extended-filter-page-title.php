a580e1bca50bc006cb6367c9808465887fdb3cf9
Metin Şaylan <metinsaylan@gmail.com>
2020-12-19 04:15:26 +0300

    Update subpages-extended-filter-page-title.php

--- subpages-extended-filter-page-title.php
+++ subpages-extended-filter-page-title.php
@@ -1,5 +1,9 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Invalid request.' );
+}
+
 // Filters page title so that Subpages Extended menu title will be used
 function shailan_page_title_filter( $title, $id  ){
 

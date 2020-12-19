8de2a0e616d16f03748473cf02ea7f1b1ef25e96
Metin Şaylan <metinsaylan@gmail.com>
2020-12-19 05:13:40 +0300

    fix doc links

--- wpa-sidebar.php
+++ wpa-sidebar.php
@@ -2,19 +2,19 @@
 Do you like <strong>Subpages Extended</strong>? Here are a few ways you can support us: 
 <ul>
     <li>⭐ Rate it on <a href="https://wordpress.org/plugins/subpages-extended/" target="_blank" >WordPress.org</a></li>
-    <li>☕ <a href="https://metinsaylan.com/live/donate/" target="_blank" >Donate</a> using Stripe</li>
-    <li>📢 <a href="https://metinsaylan.com/wordpress/plugins/subpages-widget/" target="_blank" >Spread the word</a></li>
+    <li>☕ <a href="https://wpassist.me/donate/" target="_blank" >Donate</a> using Stripe</li>
+    <li>📢 <a href="https://wpassist.me/plugins/subpages-widget/" target="_blank" >Spread the word</a></li>
 </ul>
 
-If you have any suggestions or bug reports, please <a href="https://metinsaylan.com/wordpress/plugins/subpages-widget/">write to us</a> on the <a href="https://metinsaylan.com/wordpress/plugins/subpages-widget/">plugin page</a>.
+If you have any suggestions or bug reports, please <a href="https://wpassist.me/plugins/subpages-widget/">write to us</a> on the <a href="https://wpassist.me/plugins/subpages-widget/">plugin page</a>.
 </div>
 
 <div class="card">
 Check out our other plugins:
 <ul>
-    <li>⏳ <a href="https://metinsaylan.com/wordpress/plugins/countdown/" target="_blank" >Countdown Widget</a></li>
-    <li>💰 <a href="https://metinsaylan.com/wordpress/plugins/adsense-widget/" target="_blank" >Adsense Widget</li>
-    <li>🖼️ <a href="https://metinsaylan.com/wordpress/plugins/image-banner-widget/" target="_blank" >Image/Banner Widget</a></li>
+    <li>⏳ <a href="https://wpassist.me/plugins/countdown-widget/" target="_blank" >Countdown Widget</a></li>
+    <li>💰 <a href="https://wpassist.me/plugins/adsense-widget/" target="_blank" >Adsense Widget</li>
+    <li>🖼️ <a href="https://wpassist.me/plugins/image-banner-widget/" target="_blank" >Image/Banner Widget</a></li>
 </ul>
 
 </div>
